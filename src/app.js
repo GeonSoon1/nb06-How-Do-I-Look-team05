d96a36b17413aba8c0d64707ddc94a6160eec149
geonsoon <qkrrjstns23@gmail.com>
2025-11-12 23:48:23 +0900

    ✨ feat : curationRouter.js 추가

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
 import { PORT } from './utils/constants.js';
+import curationRouter from './routers/curationRouters.js'
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-app.use(cors());
+
+app.use('/curations', curationRouter)
 
 app.listen(PORT || 3000, () => console.log('server started'));
--- src/routers/curationRouters.js
+++ src/routers/curationRouters.js
@@ -1,3 +1,9 @@
 import express from 'express';
+import { updateCuration, deleteCuration } from '../structs/curationStructs.js';
 
-// const <> = express.Router();
+const curationRouter = express.Router()
+
+curationRouter.put('/:curationId', updateCuration)
+curationRouter.delete('/:curationId', deleteCuration)
+
+export default curationRouter
