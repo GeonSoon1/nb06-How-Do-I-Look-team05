aa9718f06bb944652b0653c2fe9aa4caa05478c0
이상운 <sadqueen@isang-un-ui-MacBookPro.local>
2025-11-12 18:47:16 +0900

    파일정리

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import cors from 'cors';
 import express from 'express';
-import 'express-async-errors';
 import { PORT } from './utils/constants.js';
-import curationRouter from './routers/curationRouters.js'
+import styleRouter from './routers/styleRouter.js';
+import curationRouter from './routers/curationRouters.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/curations', curationRouter);
 
-app.use('/curations', curationRouter)
+app.use('/styles', styleRouter);
 
 app.listen(PORT || 3000, () => console.log('server started'));
