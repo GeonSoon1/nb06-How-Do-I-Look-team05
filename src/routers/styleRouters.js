e21457708cadf6c9cf4aa1d5234a40c98584fccf
이상운 <unzzang@gmail.com>
2025-11-14 14:34:46 +0900

    feat: add_passwordValidator

--- src/routers/styleRouters.js
+++ src/routers/styleRouters.js
@@ -4,14 +4,15 @@ import { createStyle, patchStyle, deleteStyle } from '../controllers/styleContro
 import { validate } from '../middlewares/styleValidator.js';
 import { upload } from '../middlewares/upload.js';
 import { createStyleCuration } from '../controllers/curationController.js';
+import { hashPassword, verifyPassword } from '../middlewares/passwordValidator.js';
 
 const router = express.Router();
 
-router.route('/').post(upload.array('images', 5), validate(CreateStyle), createStyle);
+router.route('/').post(upload.array('images', 5), validate(CreateStyle), hashPassword, createStyle);
 router
   .route('/:styleId')
-  .patch(upload.array('images', 5), validate(PatchStyle), patchStyle)
-  .delete(deleteStyle);
+  .patch(upload.array('images', 5), validate(PatchStyle), verifyPassword, patchStyle)
+  .delete(upload.none(), verifyPassword, deleteStyle);
 
 // 큐레이팅 등록
 router.post('/:styleId/curations', createStyleCuration);
