88b1c976dba28e45d7576bacc54ab8f3928b98d6
minyeok <rltkeh150@naver.com>
2025-11-15 22:11:50 +0900

    ✨ feat : 댓글 수정 기능 추가

--- src/routers/curationRouters.js
+++ src/routers/curationRouters.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { updateCuration, deleteCuration } from '../controllers/curationController.js';
 import { createComment } from '../controllers/commentController.js';
+import { commentValidator } from '../middlewares/commentValidator.js';
+import { CreateComment } from '../structs/commentStructs.js';
 const curationRouter = express.Router();
 
 curationRouter.route('/:curationId').put(updateCuration).delete(deleteCuration);
 
-curationRouter.route('/:curationId/comments').post(createComment);
+curationRouter.route('/:curationId/comments').post(commentValidator(CreateComment), createComment);
 
 export default curationRouter;
