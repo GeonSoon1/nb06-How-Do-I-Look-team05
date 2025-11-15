1b1e9234adc4f77a145855a120b6e08a4edd1f18
minyeok <rltkeh150@naver.com>
2025-11-15 16:45:47 +0900

    feat:댓글 기능 개발 중

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -3,11 +3,13 @@ import { prisma } from '../utils/prisma.js';
 export const createComment = async (req, res) => {
   const curationId = parseInt(req.params.curationId, 10);
   const { ...commentData } = req.body;
-
+  console.log(curationId);
   const comment = await prisma.curationComment.create({
     data: {
       ...commentData,
-      curationId
+      curation: {
+        connect: curationId
+      }
     }
   });
   res.status(201).send(comment);
