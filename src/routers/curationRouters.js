29d495161f86ae05060348d243e668dbc3b364eb
geonsoon <qkrrjstns23@gmail.com>
2025-11-18 19:21:54 +0900

    ✨ feat : curation_form-data 들어온 데이터 num으로 수정

--- src/routers/curationRouters.js
+++ src/routers/curationRouters.js
@@ -6,14 +6,14 @@ import { commentValidator } from '../middlewares/commentValidator.js';
 import { PatchCuration } from '../structs/curationStructs.js';
 import { CreateComment } from '../structs/commentStructs.js';
 import { verifyPassword, verifyStylePassword } from '../middlewares/passwordValidator.js';
-import { textParser } from '../middlewares/formDataParser.js';
-import { asyncHandler } from '../middlewares/asyncHandler.js';
+import { upload } from '../middlewares/formDataParser.js'; // 여기서 upload 가져옴
+
 const curationRouter = express.Router();
 
 curationRouter
   .route('/:curationId')
-  .put(curationValidator(PatchCuration), verifyPassword, updateCuration)
-  .delete(verifyPassword, deleteCuration);
+  .put(upload.none(), curationValidator(PatchCuration), verifyPassword, updateCuration)
+  .delete(upload.none(), verifyPassword, deleteCuration);
 
 curationRouter
   .route('/:curationId/comments')
--- src/structs/curationStructs.js
+++ src/structs/curationStructs.js
@@ -4,20 +4,20 @@ export const CreateCuration = s.object({
   nickname: s.size(s.string(), 1, 10),
   content: s.string(),
   password: s.string(),
-  trendy: s.max(s.min(s.integer(), 0), 10),
-  personality: s.max(s.min(s.integer(), 0), 10),
-  practicality: s.max(s.min(s.integer(), 0), 10),
-  costEffectiveness: s.max(s.min(s.integer(), 0), 10)
+  trendy: s.max(s.min(s.number(), 0), 10),
+  personality: s.max(s.min(s.number(), 0), 10),
+  practicality: s.max(s.min(s.number(), 0), 10),
+  costEffectiveness: s.max(s.min(s.number(), 0), 10)
 });
 
 export const PatchCuration = s.object({
   nickname: s.size(s.string(), 1, 10),
   content: s.string(),
   password: s.string(),
-  trendy: s.max(s.min(s.integer(), 0), 10),
-  personality: s.max(s.min(s.integer(), 0), 10),
-  practicality: s.max(s.min(s.integer(), 0), 10),
-  costEffectiveness: s.max(s.min(s.integer(), 0), 10)
+  trendy: s.max(s.min(s.number(), 0), 10),
+  personality: s.max(s.min(s.number(), 0), 10),
+  practicality: s.max(s.min(s.number(), 0), 10),
+  costEffectiveness: s.max(s.min(s.number(), 0), 10)
 });
 
 // export const DeleteCuration = s.object({
