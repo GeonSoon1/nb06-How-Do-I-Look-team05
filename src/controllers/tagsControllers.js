28969ed196a89a9690285cfd0a269568d218a824
minyeok <rltkeh150@naver.com>
2025-11-18 11:44:19 +0900

    ♻️ chore(update) :dev 최종 수정

--- src/controllers/tagsControllers.js
+++ src/controllers/tagsControllers.js
@@ -1,14 +1,18 @@
 import { prisma } from '../utils/prisma.js';
 
-export const getTags = async (req, res) => {
-  const tags = await prisma.tag.findMany({
-    orderBy: [{ styleCount: 'desc' }, { clickCount: 'desc' }],
-    take: 10,
-    select: { tag: true },
-  });
+export const getTags = async (req, res, next) => {
+  try {
+    const tags = await prisma.tag.findMany({
+      orderBy: [{ styleCount: 'desc' }, { clickCount: 'desc' }],
+      take: 10,
+      select: { tag: true }
+    });
 
-  const response = {
-    tag: tags.map((data) => data.tag),
-  };
-  res.status(200).send(response);
+    const response = {
+      tag: tags.map((data) => data.tag)
+    };
+    res.status(200).send(response);
+  } catch (e) {
+    next(e);
+  }
 };
